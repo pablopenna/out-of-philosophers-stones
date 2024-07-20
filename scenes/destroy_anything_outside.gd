147b26c41cbbdaeb331f44a27ecbf466b2e48986
Pablo <pablo_penna7@hotmail.com>
2024-07-20 20:35:58 +0100

    Add functionality to destroy any projectiles or enemies outside of the screen to fix performance

---
+++ scenes/destroy_anything_outside.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+func _ready() -> void:
+	self.area_exited.connect(_destroy)
+	self.body_exited.connect(_destroy)
+
+func _destroy(node: Node):
+	print("destroying ", node)
+	node.queue_free()
