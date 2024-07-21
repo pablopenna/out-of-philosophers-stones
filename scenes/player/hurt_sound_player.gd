8b354ecebadbea734087736c65cb90f78ae634a6
Pablo <pablo_penna7@hotmail.com>
2024-07-21 21:40:19 +0100

    Add component that can randomize pitch of sound played

--- scenes/player/hurt_sound_player.gd
+++ scenes/player/hurt_sound_player.gd
@@ -1,4 +1,5 @@
-extends AudioStreamPlayer
+extends RandomPitchAudioStreamPlayer
 
 func _on_health_points_manager_health_decreased(_new_health_amount: int, _previous_health_amount: int) -> void:
+	randomize_pitch()
 	play()
---
+++ scenes/utils/random_pitch.gd
@@ -0,0 +1,8 @@
+class_name RandomPitchAudioStreamPlayer extends AudioStreamPlayer
+
+@export var max_pitch: float = 1.1
+@export var min_pitch: float = 0.9
+
+func randomize_pitch():
+	var new_pitch: float = randf_range(min_pitch, max_pitch)
+	self.pitch_scale = new_pitch
