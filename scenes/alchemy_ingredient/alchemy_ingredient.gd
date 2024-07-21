dce582b80a3e824fa0243140b4f7fa04da449d30
Pablo <pablo_penna7@hotmail.com>
2024-07-21 22:17:16 +0100

    Add AlchemyIngredient scene

---
+++ scenes/alchemy_ingredient/alchemy_ingredient.gd
@@ -0,0 +1,9 @@
+class_name AlchemyIngredient extends Area2D
+
+enum IngredientType {
+	HAND,
+	EYE,
+	BONE
+}
+
+@export var ingredient: IngredientType
---
+++ scenes/alchemy_ingredient/ingredient_sprite.gd
@@ -0,0 +1,15 @@
+extends Sprite2D
+
+@export var alchemy_material: AlchemyIngredient
+@export var hand_texture: CompressedTexture2D
+@export var eye_texture: CompressedTexture2D
+@export var bone_texture: CompressedTexture2D
+
+func _ready():
+	match alchemy_material.ingredient:
+		AlchemyIngredient.IngredientType.HAND:
+			self.texture = hand_texture
+		AlchemyIngredient.IngredientType.EYE:
+			self.texture = eye_texture
+		AlchemyIngredient.IngredientType.BONE:
+			self.texture = bone_texture
