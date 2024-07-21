4d9256249841b5bfa17feee695dce52c9dcf12d4
Pablo <pablo_penna7@hotmail.com>
2024-07-21 21:39:31 +0100

    fix missing class definition error after cleanup

--- scenes/common/entity.gd
+++ scenes/common/entity.gd
@@ -1,11 +1,5 @@
 class_name Entity extends CharacterBody2D
 
-@onready var facing_direction: Vector2 = Vector2.DOWN
-
 func _physics_process(_delta: float) -> void:
-	_update_facing_direction()
 	# "move_and_slide" already takes delta time into account
 	move_and_slide()
-	
-func _update_facing_direction() -> void:
-	facing_direction = FacingDirectionUtils.getFacingDirectionFromVelocityWithDefault(velocity, facing_direction)
